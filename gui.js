6d1b4aece06780a210872f37f08fbda48c484a62
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2025-04-19 09:36:38 +0600

    Update gui.js
    Updated gui to now use internet assets

--- gui.js
+++ gui.js
@@ -6,6 +6,13 @@ let jsonURL = "data/apartment1.json";
 fetch(jsonURL)
   .then(res => res.json())
   .then(config => {
+    // Load video dynamically from JSON
+    if (config.video) {
+      videoEl.setAttribute("src", config.video);
+      videoEl.load();
+      videoEl.play();
+    }
+
     clearMenu();
     setupBasicControls(config);
     setupSceneButtons(config);
@@ -44,6 +51,8 @@ function setupBasicControls(config) {
 }
 
 function setupSceneButtons(config) {
+  if (!config.scenes) return;
+
   let sceneGroup = document.createElement("a-entity");
   sceneGroup.setAttribute("position", "0 -1.5 -3");
 
@@ -59,6 +68,8 @@ function setupSceneButtons(config) {
 }
 
 function setupEndOptions(config) {
+  if (!config.endOptions) return;
+
   videoEl.addEventListener("ended", () => {
     let endGroup = document.createElement("a-entity");
     endGroup.setAttribute("position", "0 1 -2.5");
@@ -66,11 +77,13 @@ function setupEndOptions(config) {
     config.endOptions.forEach((option, i) => {
       let y = -i * 0.5;
       let buttonEl = createButton(option.label, `0 ${y} 0`, () => {
+        clearMenu();
+
         if (option.nextVideo) {
-          videoEl.src = "videos/" + option.nextVideo;
+          videoEl.setAttribute("src", option.nextVideo);
           videoEl.load();
           videoEl.play();
-          clearMenu();
+
           fetch("data/" + option.nextVideo.replace(".mp4", ".json"))
             .then(res => res.json())
             .then(cfg => {
@@ -78,10 +91,13 @@ function setupEndOptions(config) {
               setupSceneButtons(cfg);
               setupEndOptions(cfg);
             });
+
         } else if (option.menu) {
           alert("Opening menu: " + option.menu);
+          // Add your menu loading logic here later
         }
       });
+
       endGroup.appendChild(buttonEl);
     });
 
